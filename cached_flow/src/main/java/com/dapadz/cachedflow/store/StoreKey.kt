90022bf2e6f81c64a89ad3369029360b6cdc1c35
Igor Zakharov <i.zakharov@l-tech.ru>
2025-05-29 14:05:16 +0300

    Make StoreKey fields public
    The `name` and `type` fields in the `StoreKey` class are now public, allowing external access to these properties.

--- cached_flow/src/main/java/com/dapadz/cachedflow/store/StoreKey.kt
+++ cached_flow/src/main/java/com/dapadz/cachedflow/store/StoreKey.kt
@@ -3,6 +3,6 @@ package com.dapadz.cachedflow.store
 import kotlin.reflect.KClass
 
 class StoreKey<T : Any>(
-    private val name: String,
-    private val type: KClass<T>
+    val name: String,
+    val type: KClass<T>
 )
